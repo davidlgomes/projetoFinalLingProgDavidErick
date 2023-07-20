b17113fd9840f296ec878bea1c63187c92f31f96
David Lopes Gomes <gomes.david.9@gmail.com>
2023-07-20 01:26:55 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
+// main.cpp
 #include <iostream>
 #include "criarTabela.h"
 #include "crud.h"
 using namespace std;
+
 int main()
 {
     Crud crud;
@@ -16,35 +18,30 @@ int main()
         cout << "3 - Atualizar um componente do estoque " << endl;
         cout << "4 - Remover um componente do estoque" << endl;
         cout << "5 - Help " << endl;
-        cout << "6 - Sair " << endl
-             << endl;
+        cout << "6 - Sair " << endl << endl;
         cout << ">>";
         cin >> opcoesMenu;
         switch (opcoesMenu)
         {
         case 1:
-            cout << "1 - Pesquisar Item do estoque" << endl
-                 << endl;
+            cout << "1 - Pesquisar Item do estoque" << endl << endl;
             crud.pesquisarComponente();
             break;
         case 2:
-            cout << "2 - Adicionar componente ao estoque" << endl
-                 << endl;
+            cout << "2 - Adicionar componente ao estoque" << endl << endl;
             crud.adicionarComponente();
             break;
         case 3:
-            cout << "3 - Atualizar um componente do estoque " << endl
-                 << endl;
+            cout << "3 - Atualizar um componente do estoque " << endl << endl;
             crud.atualizarComponente();
             break;
         case 4:
-            cout << "4 - Remover um componente do estoque" << endl
-                 << endl;
+            cout << "4 - Remover um componente do estoque" << endl << endl;
             crud.excluirComponente();
             break;
         case 5:
             cout << "***************************************" << endl;
-            cout << "********** Help - Funcionamento do Programa **********" << endl;
+            cout << "*   Help - Funcionamento do Programa  *" << endl;
             cout << "***************************************" << endl;
             cout << "O programa é um sistema de gerenciamento de estoque de componentes." << endl;
             cout << "Ele permite executar diferentes operações relacionadas aos componentes armazenados no estoque." << endl;
@@ -67,12 +64,12 @@ int main()
             break;
         default:
             cout << "Opção Inválida!" << endl;
-            cout << "Por favor escolher uma opção válida!" << endl
-                 << endl;
+            cout << "Por favor escolher uma opção válida!" << endl << endl;
             break;
         }
 
         cin.ignore();
     } while (!saidaMenu);
+
     return 0;
 }
